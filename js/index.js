593037ea4b506f9470d3bac05e2bc796a498b2b1
Jiangheliu1996 <51378741+Jiangheliu1996@users.noreply.github.com>
2019-12-02 00:00:01 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function $(select){
 
 function getmusicList(callback){
     var xhr = new XMLHttpRequest()
-    xhr.open('GET', '/music.json', true)
+    xhr.open('GET', '/musicPlayer/music.json', true)
     xhr.onload = function(){
         if((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304){
             callback(JSON.parse(xhr.responseText))
